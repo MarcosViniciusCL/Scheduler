4fbc2fd843d5ed653a3cb7f37733ea1fc7a34ecc
marcosviniciuscl <marcosviniciuscl@outlook.com>
2019-06-01 09:21:41 -0300

    Blabla

--- app/src/main/java/com/example/scheduler/MainActivity.kt
+++ app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.scheduler
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
